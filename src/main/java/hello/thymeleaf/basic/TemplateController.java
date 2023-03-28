f6b31ae9d120bb371e899efa1d8b95aaea44c594
vpfl95 <k_dh610@naver.com>
2023-03-28 22:41:03 +0900

    템플릿 레이아웃2

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -11,4 +11,14 @@ public class TemplateController {
     public String template(){
         return "template/fragment/fragmentMain";
     }
+
+    @GetMapping("layout")
+    public String layout(){
+        return "template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend(){
+        return  "template/layoutExtend/layoutExtendMain";
+    }
 }
